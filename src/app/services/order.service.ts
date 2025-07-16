368dde179feaf467889cb2cf8ec6cbddc113fe7f
jefferson.sotto <jeffersontads@hotmail.com>
2025-07-16 00:41:19 -0300

    wip fix url orders in order service

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -6,7 +6,7 @@ import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class OrderService {
-  private apiUrl = `${environment.apiUrl}/orders`;
+  private apiUrl = `${environment.apiUrl}/api/pedidos`;
 
   constructor(private http: HttpClient) {}
 
