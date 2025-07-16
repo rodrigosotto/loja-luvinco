430ab7189d4da983a2af307b68f6cc61b7f45fe7
jefferson.sotto <jeffersontads@hotmail.com>
2025-07-16 00:02:55 -0300

    wip add environment api url in product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Product } from '../models/product.model';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
 export class ProductService {
+  private apiUrl = `${environment.apiUrl}/products`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts(filters?: {
+    category?: string;
+    brand?: string;
+  }): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl, { params: filters });
+  }
 }
